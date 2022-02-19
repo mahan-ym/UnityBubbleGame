6d59c946dc43b37b3297ba23ef80080c71ea1cfb
mahan <mahan.ymt@gmail.com>
2022-02-19 15:15:16 +0330

    refactor: remove DontDestroyOnLoad as it was causing error and shouldn't be called in Awake function

--- Assets/Script/Public/SpriteManager.cs
+++ Assets/Script/Public/SpriteManager.cs
@@ -26,7 +26,6 @@ public class SpriteManager : MonoBehaviour {
 	void Awake()
 	{
 		Instance = this;
-		DontDestroyOnLoad (gameObject);
 
 		for (int i=0; i<spriteList.Count; ++i) {
 			spriteTable.Add(spriteList[i].name,spriteList[i]);
